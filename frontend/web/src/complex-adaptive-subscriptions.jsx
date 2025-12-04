a887ac593359f8c455e4732aa28d5a465659280d
wfiemauRMp83r5C3500NicholasMoran <zhorik-l-825330067@rambler.ru>
2025-12-04 11:45:46 +0800

    add hardhat.config.ts, demo-template-environment.py, complex-adaptive-subscriptions.jsx

---
+++ frontend/web/src/complex-adaptive-subscriptions.jsx
@@ -0,0 +1,3 @@
+export const config = { runtime: 'edge' };
+
+export default function utils-mock-factory() { return <div>Component</div>; }
---
+++ frontend/web/src/provider/demo-template-environment.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\provider\demo-template-environment.py
+// Generated at: 2025-10-10T05:27:52.766Z
